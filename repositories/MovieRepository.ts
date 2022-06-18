ff68a96af1a9854f20238a9b1461f6e575787a9f
bluewave41 <megaman22342@live.ca>
2022-06-17 20:33:11 -0700

    Added update route and test

--- repositories/MovieRepository.ts
+++ repositories/MovieRepository.ts
@@ -5,18 +5,27 @@ async function createMovie(movie: MovieModel) {
         .insert(movie);
 }
 
-async function deleteMovie(id: number) {
-    await MovieModel.query().delete()
-        .where('movieId', id);
-}
-
 async function getMovie(id: number) {
     return await MovieModel.query()
         .findOne('id', id);
+}
+
+async function updateMovie(movie: MovieModel) {
+    /*
+        The JSON schema is being checked in the route so patch is used here over update as per below
+        https://github.com/Vincit/objection.js/issues/965
+    */
+    return await MovieModel.query()
+        .patchAndFetch(movie);
+}
+
+async function deleteMovie(id: number) {
+    await MovieModel.query().delete()
+        .where('movieId', id);
 }
 
 function fromObject(movieObject: object) {
     return MovieModel.fromJson(movieObject);
 }
 
-export default { createMovie, deleteMovie, getMovie, fromObject }
+export default { createMovie, getMovie, updateMovie, deleteMovie, fromObject }
